f9cf2e3f6ac822ce7a930c12468af93f82713182
Josh Chasky <jchasky@gmail.com>
2019-09-18 08:57:34 -0400

    Ensure the proper Google Assistant prop is inherited
    * Also add ANGLE prop
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device-common.mk
+++ device-common.mk
@@ -151,3 +151,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Set thermal warm reset
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.thermal_warmreset = true \
+
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true \
+    ro.gfx.angle.supported=true
