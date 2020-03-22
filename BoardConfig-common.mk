1228a48efedecdaeb5d15fe4e4665d22cafba9f3
slothdabski <allthatsleft169@gmail.com>
2020-03-22 12:59:58 -0500

    bluecross: disable artifact path requirements
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -22,6 +22,7 @@ include build/make/target/board/BoardConfigMainlineCommon.mk
 TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_INFO_FILE := device/google/crosshatch/board-info.txt
 USES_DEVICE_GOOGLE_B1C1 := true
+DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
