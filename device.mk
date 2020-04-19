db18017dc7d1330cfd3cce827e1e4917cba2f7a0
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    bluecross: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I1af1fb327cf8a5afa73cb6b6de92264b26dca091
    Signed-off-by: nysascape <jago@nysascape.tech>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device.mk
+++ device.mk
@@ -894,3 +894,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     ro.sf.blurs_are_expensive=1
 
+# Force triple frame buffers
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
