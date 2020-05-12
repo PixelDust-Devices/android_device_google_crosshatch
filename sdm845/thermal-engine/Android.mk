31733132d4a03a162dd0428fbb05c7c058e0ea38
spezi77 <spezi7713@gmx.net>
2020-05-12 19:14:34 +0200

    crosshatch: Nuke thermal-engine headers.
    error: duplicate header copies are no longer allowed.
    For more information about headers, see: https://android.googlesource.com/platform/build/soong/+/master/docs/best_practices.md#headers.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- sdm845/thermal-engine/Android.mk
+++ sdm845/thermal-engine/Android.mk
@@ -2,6 +2,3 @@
 # @file device/qcom/common/thermal-engine/Android.mk
 # @Makefile for installing thermal-engine client header on Android.
 ################################################################################
-
-LOCAL_PATH:= $(call my-dir)
-
