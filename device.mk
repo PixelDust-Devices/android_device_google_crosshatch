b67ffbdf769dda3d3fd5b56f5ed79689ece011f6
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:58:27 -0700

    bluecross: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ia3968313601d2b04ce50c01ee61dc9944ee26187
    Signed-off-by: nysascape <jago@nysascape.tech>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device.mk
+++ device.mk
@@ -891,5 +891,6 @@ include hardware/google/pixel/thermal/device.mk
 
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
 
