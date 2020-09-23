5104abbb1c5f6b472150b64b1ac94b87b8a4508d
spezi77 <spezi7713@gmx.net>
2020-09-23 10:35:26 +0200

    crosshatch: Workaround qcom's broken header copy.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+
 include build/make/target/board/BoardConfigMainlineCommon.mk
 
 TARGET_BOARD_PLATFORM := sdm845
