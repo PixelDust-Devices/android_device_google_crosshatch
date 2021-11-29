860c3b16de1b8dbc2900d182e9ce8b9a4f639f8d
spezi77 <spezi7713@gmx.net>
2021-11-29 19:03:39 +0100

    Pixel identification: Change build fp for AND 12 (November 2021).
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- aosp_blueline.mk
+++ aosp_blueline.mk
@@ -64,12 +64,6 @@ PRODUCT_DEVICE := blueline
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
 
-# Match stock product info
-PRODUCT_OVERRIDE_INFO := true
-PRODUCT_OVERRIDE_NAME := blueline
-PRODUCT_OVERRIDE_FINGERPRINT := google/blueline/blueline:11/RQ3A.210805.001.A1/7474174:user/release-keys
-PRODUCT_OVERRIDE_DESC := blueline-user 11 RQ3A.210805.001.A1 7474174 release-keys
-
 LOCAL_PATH := device/google/crosshatch
 
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/blueline.prop
--- aosp_crosshatch.mk
+++ aosp_crosshatch.mk
@@ -67,12 +67,6 @@ PRODUCT_DEVICE := crosshatch
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3 XL
 
-# Match stock product info
-PRODUCT_OVERRIDE_INFO := true
-PRODUCT_OVERRIDE_NAME := crosshatch
-PRODUCT_OVERRIDE_FINGERPRINT := google/crosshatch/crosshatch:11/RQ3A.210805.001.A1/7474174:user/release-keys
-PRODUCT_OVERRIDE_DESC := crosshatch-user 11 RQ3A.210805.001.A1 7474174 release-keys
-
 LOCAL_PATH := device/google/crosshatch
 
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/crosshatch.prop
