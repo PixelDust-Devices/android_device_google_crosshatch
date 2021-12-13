7ed3756261ac152815e376da2073f44e6cec6cf5
spezi77 <spezi7713@gmx.net>
2021-12-13 12:28:01 +0100

    crosshatch: Include CAF specific Vendor Interface Manifest if needed.
    Imported from: https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/core-utils/tree/vendor_framework_compatibility_matrix.xml?h=LA.QSSI.12.0.r1-04900-qssi.0
    
    This is needed to satisfy the vintf checker when compiling QTI wifi HALs:
    vendor.qti.hardware.wifi.hostapd@1.2::IHostapdVendor/default
    vendor.qti.hardware.wifi.supplicant@2.2::ISupplicantVendor/default
    
    BTW, our build flag is defined here: https://github.com/pixeldust-project-caf/android_vendor_pixeldust/commit/3947da62e1a141ce509f7e2b5da267fadf942c4e#diff-6c784b15eacf63d2b12190534f7ea088db918bd116600484803be467a1816543R20
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -298,6 +298,12 @@ DEVICE_MANIFEST_FILE := device/google/crosshatch/manifest.xml
 DEVICE_MATRIX_FILE := device/google/crosshatch/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/google/crosshatch/device_framework_matrix.xml
 
+# Add CAF specific Vendor Interface Manifest if needed
+ifneq (,$(filter caf, $(BUILD_VERSION_CODE)))
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    device/google/crosshatch/vendor_framework_compatibility_matrix.xml
+endif
+
 # Userdebug only Vendor Interface Manifest
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/google/crosshatch/framework_manifest_userdebug.xml
