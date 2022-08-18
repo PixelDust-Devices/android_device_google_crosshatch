6b2f9438a7815309a8abbbec50cb35ca228056fc
ReallySnow <reallysnow233@gmail.com>
2022-08-18 17:57:43 +0800

    b1c1: Add shim for missing symbol in libsecureuisvc_jni.so
    * java.lang.UnsatisfiedLinkError: dlopen failed: cannot locate symbol "_ZN7android21SurfaceComposerClient11Transaction5applyEb" referenced by "/product/lib64/libsecureuisvc_jni.so"...
    
    Change-Id: I7c1b677d7a0f31f7ab46ecf7d6e595817722b072
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device.mk
+++ device.mk
@@ -1067,7 +1067,8 @@ PRODUCT_PACKAGES += \
     android.hardware.identity-V3-ndk_platform.vendor:64 \
     android.hardware.keymaster-V3-ndk_platform.vendor:64 \
     android.hardware.power-V1-ndk_platform.vendor:64 \
-    android.hardware.rebootescrow-V1-ndk_platform.vendor:64
+    android.hardware.rebootescrow-V1-ndk_platform.vendor:64 \
+    libgui_shim
 
 # Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
