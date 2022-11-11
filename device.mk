abd0b0440202a4bf77a27267bf11b7417c99e583
ReallySnow <reallysnow233@gmail.com>
2022-11-11 21:20:55 +0800

    b1c1: Track GoogleCamera 8.3
    * Gcam 8.6 doesn't work in bluecross family,
      so we can use version 8.3 from play store
    
    Change-Id: I058f3b92049d1bc6ec63adb8ea0abee35995777d
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device.mk
+++ device.mk
@@ -1021,6 +1021,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lib-secureuishim
 
+# Camera
+PRODUCT_PACKAGES += \
+    GcamPrebuilt
+
 # Camera Extensions
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
