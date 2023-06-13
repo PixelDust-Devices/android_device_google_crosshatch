16afd9748e80468154167037f3f2a6f44d10deab
Chris Crump <chriscrawford893@gmail.com>
2023-06-13 21:54:52 +0200

    b1c1: Add AOSP Bluetooth soong namespace
    Change-Id: Id60d0b1a6c15d203e9846e995b1d00357045bce7
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- device.mk
+++ device.mk
@@ -1075,3 +1075,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Set system properties identifying the chipset
 PRODUCT_VENDOR_PROPERTIES += ro.soc.manufacturer=Qualcomm
 PRODUCT_VENDOR_PROPERTIES += ro.soc.model=SDM845
+
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    packages/modules/Bluetooth/android/app
